bc89a9fb045e70b249ca7f0e90f84750cbea2cfe
Mr Nate <emailaddrress>
2023-04-12 20:52:41 -0500

    init app

---
+++ shenanigans/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import UploadPage from './input/Shenanigans-with-Nate';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UploadPage></UploadPage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
